8f5a44b9e4dabcdbc76fa90149ee837475469a9f
Darragh Hewitt <darragh@Darraghs-MacBook-Air.local>
2019-08-26 12:31:36 +0100

    Sound assets and functionality

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ var matchedCards = []
 
 var board = document.getElementById("card-game")
 
+var clickSound = new Audio;
+clickSound.src = "sounds/click.mp3";
+
+var correctSound = new Audio;
+correctSound.src = "sounds/correct.mp3";
+
+var wrongSound = new Audio;
+wrongSound.src = "sounds/wrong.mp3"
+
+
+function playWrong(){
+    wrongSound.play();
+}
+function playCorrect(){
+    correctSound.play();
+}
 
 function startGame() {
     cards.forEach(card =>card.addEventListener('click', flipCard));
+    shuffle();
 }
 
+function shuffle(){
+    cards.forEach(card=>{
+        let randomNum= Math.floor(Math.random()*20);
+        card.style.order = randomNum;
+    })
+}
 
 function flipCard() {
+    clickSound.play();
     let clickedCard = this;
     if (clickedCard.classList.contains("card")) {
      clickedCard.classList.add("flip");
@@ -21,8 +45,11 @@ function flipCard() {
         if (cardHand.length === 2) {
             board.classList.add('pause')
             if (cardHand[0].dataset.framework === cardHand[1].dataset.framework){
+                setTimeout(playCorrect, 800);
                 matched()
              } else{
+                 setTimeout(playWrong, 800)
+                 setTimeout(notMatched, 700)
             }
         }       
 }
@@ -46,3 +73,5 @@ function notMatched() {
 
 }
 startGame();
+
+
