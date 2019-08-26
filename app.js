cfb98f3152ae7a6a5f58ec7f6915a498858a7e6d
Darragh Hewitt <darragh@Darraghs-MacBook-Air.local>
2019-08-26 10:14:29 +0100

    Matching logic added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const cards = document.querySelectorAll('.card');
 
+var cardHand = []
+
+var matchedCards = []
+
 var board = document.getElementById("card-game")
+
+
+function startGame() {
+    cards.forEach(card =>card.addEventListener('click', flipCard));
+}
+
 
 function flipCard() {
     let clickedCard = this;
@@ -8,4 +18,31 @@ function flipCard() {
      clickedCard.classList.add("flip");
      cardHand.push(clickedCard);
         }
-    }
+        if (cardHand.length === 2) {
+            board.classList.add('pause')
+            if (cardHand[0].dataset.framework === cardHand[1].dataset.framework){
+                matched()
+             } else{
+            }
+        }       
+}
+
+function matched() {
+cardHand[0].classList.add("matched");
+cardHand[1].classList.add("matched");
+matchedCards.push(cardHand[0]);
+matchedCards.push(cardHand[1]);
+cardHand = [];
+console.log(matchedCards);
+
+board.classList.remove('pause');
+}
+
+function notMatched() {
+    cardHand[0].classList.remove("flip");
+    cardHand[1].classList.remove("flip");
+    cardHand = []
+    board.classList.remove('pause');
+
+}
+startGame();
