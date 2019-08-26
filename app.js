54f664f6b528e235af23309b57d49c28c44e5941
Darragh Hewitt <darragh@Darraghs-MacBook-Air.local>
2019-08-26 15:16:57 +0100

    Added restart function

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+(function(){
+
 const cards = document.querySelectorAll('.card');
+
+let moves = 0;
+var movesCounter = document.querySelector(".moves");
 
 var cardHand = []
 
 var matchedCards = []
 
 var board = document.getElementById("card-game")
+
+restartButton = document.querySelector('.fa').addEventListener('click', restart);
 
 var clickSound = new Audio;
 clickSound.src = "sounds/click.mp3";
@@ -15,6 +22,19 @@ correctSound.src = "sounds/correct.mp3";
 var wrongSound = new Audio;
 wrongSound.src = "sounds/wrong.mp3"
 
+var winSound = new Audio;
+winSound.src = "sounds/win.mp3";
+
+
+
+function movesNum() {
+    moves++;
+    if (moves === 1) {
+        movesCounter.innerHTML = `1  Move`;
+    } else {
+        movesCounter.innerHTML = `${moves}  Moves`;
+    }
+}
 
 function playWrong(){
     wrongSound.play();
@@ -22,10 +42,28 @@ function playWrong(){
 function playCorrect(){
     correctSound.play();
 }
+function playWin(){
+    winSound.play();
+}
 
 function startGame() {
     cards.forEach(card =>card.addEventListener('click', flipCard));
     shuffle();
+}
+
+function restart() {
+    for (var i in matchedCards) {
+        matchedCards[i].classList.remove("matched", "flip");
+      }
+    for (var i in cardHand) {
+        cardHand[i].classList.remove("matched", "flip");
+      }
+    moves = 0;
+    movesCounter.innerHTML = `0 Moves`;
+    cardHand = []
+    matchedCards = []
+    setTimeout(shuffle, 700)
+
 }
 
 function shuffle(){
@@ -44,6 +82,7 @@ function flipCard() {
         }
         if (cardHand.length === 2) {
             board.classList.add('pause')
+            movesNum();
             if (cardHand[0].dataset.framework === cardHand[1].dataset.framework){
                 setTimeout(playCorrect, 800);
                 matched()
@@ -60,9 +99,10 @@ cardHand[1].classList.add("matched");
 matchedCards.push(cardHand[0]);
 matchedCards.push(cardHand[1]);
 cardHand = [];
-console.log(matchedCards);
-
 board.classList.remove('pause');
+if (matchedCards.length === 20) {
+    setTimeout(playWin, 1000)
+}
 }
 
 function notMatched() {
@@ -73,5 +113,4 @@ function notMatched() {
 
 }
 startGame();
-
-
+}());
